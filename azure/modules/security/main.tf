1918fcc231c7d36a7c7f32c3666454cef3dd7abf
Pseudolukian <pynanist@gmail.com>
2024-02-23 17:43:46 +0100

    Open 53,80, 22 ports

--- azure/modules/security/main.tf
+++ azure/modules/security/main.tf
@@ -16,14 +16,50 @@ resource "azurerm_network_security_group" "allow_all" {
   }
 
   security_rule {
-    access                     = "Allow"
-    direction                  = "Inbound"
-    name                       = "tls"
+    name                       = "allow_tls"
     priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
     source_address_prefix      = "*"
     destination_port_range     = "443"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow_dns"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "53"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow_http"
+    priority                   = 130
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow_icmp"
+    priority                   = 140
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Icmp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 }
